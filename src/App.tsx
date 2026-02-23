572debcafeae42740d1f0a055a20f9cd25effde8
Muskan Khandelwal <muskan.khandelwal@intimetec.com>
2026-02-23 08:40:54 +0530

    Updated category and products

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navbar";
-import { Home, ComponentDemo, Cart, Login, Signup } from "./pages";
+import { Home, ComponentDemo, Login, Signup, CheckoutPage } from "./pages";
 
 function App() {
   const location = useLocation();
@@ -13,8 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/components" element={<ComponentDemo />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Cart />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
